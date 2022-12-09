41f4733d66ea3e40152e25e01974b1d235a0723c
quanghv2000 <quanghv2000.dev@gmail.com>
2022-12-09 09:02:18 +0700

    Update

--- study project/backend/src/orm.config.ts
+++ study project/backend/src/orm.config.ts
@@ -11,30 +11,14 @@ function ormConfig(): TypeOrmModuleOptions {
         MIGRATIONS_RUN: true,
     };
 
-    // let ormconfig: TypeOrmModuleOptions = {
-    //     name: 'default',
-    //     type: 'mysql',
-    //     database: 'noi_that',
-    //     host: 'localhost',
-    //     port: 3306,
-    //     username: 'root',
-    //     password: '123456789',
-    //     logging: false,
-    //     synchronize: true,
-    //     entities: commonConf.ENTITIES,
-    //     migrations: commonConf.MIGRATIONS,
-    //     cli: commonConf.CLI,
-    //     migrationsRun: commonConf.MIGRATIONS_RUN,
-    // };
-
     let ormconfig: TypeOrmModuleOptions = {
         name: 'default',
         type: 'mysql',
-        database: 'railway',
-        host: 'containers-us-west-34.railway.app',
-        port: 7179,
+        database: 'noi_that',
+        host: 'localhost',
+        port: 3306,
         username: 'root',
-        password: 'VcSTokMJcFCPYLT4xwk1',
+        password: '123456789',
         logging: false,
         synchronize: true,
         entities: commonConf.ENTITIES,
@@ -42,6 +26,22 @@ function ormConfig(): TypeOrmModuleOptions {
         cli: commonConf.CLI,
         migrationsRun: commonConf.MIGRATIONS_RUN,
     };
+
+    // let ormconfig: TypeOrmModuleOptions = {
+    //     name: 'default',
+    //     type: 'mysql',
+    //     database: 'railway',
+    //     host: 'containers-us-west-34.railway.app',
+    //     port: 7179,
+    //     username: 'root',
+    //     password: 'VcSTokMJcFCPYLT4xwk1',
+    //     logging: false,
+    //     synchronize: true,
+    //     entities: commonConf.ENTITIES,
+    //     migrations: commonConf.MIGRATIONS,
+    //     cli: commonConf.CLI,
+    //     migrationsRun: commonConf.MIGRATIONS_RUN,
+    // };
 
     return ormconfig;
 }
